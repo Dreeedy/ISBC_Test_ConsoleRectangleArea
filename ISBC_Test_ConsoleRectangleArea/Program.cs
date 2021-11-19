c20efc55ad75bbe7751d52a45d59a76bdc44dd92
Dreeedy <dreeedyone@gmail.com>
2021-11-19 18:40:33 +0300

    Add project files.

---
+++ ISBC_Test_ConsoleRectangleArea/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISBC_Test_ConsoleRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
